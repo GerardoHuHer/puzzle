16013ae5c6da64b25648aa96b68c1f932f0ff854
GerardoHuHer <gerardohuerta1502@gmail.com>
2024-09-12 00:42:35 -0600

    Se añade funcionamieto de seleccionar cada cirN por separado, cambiar el color de los elementos de este

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,24 +1,58 @@
-// Arreglos
-var c1 = ["1", "2", "3", "4", "5", "6"];
+// Posibilidades.
+var c1 = [1, 2, 3, 4, 5, 6];
 var c2 = [1, 2, 3, 4, 5, 6, 7, 8];
 var c3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var c4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 var c5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+// Contadores de ángulos
 var rotationAngle1 = 0;
 var rotationAngle2 = 0;
 var rotationAngle3 = 0;
 var rotationAngle4 = 0;
 var rotationAngle5 = 0;
 
-function shuffleArray(array) {
-  let shuffledArray = array.slice();
+// Id circulo
+var id;
 
-  for (let i = shuffledArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1)); // Obtener un índice aleatorio
-    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]; // Intercambiar elementos
+// Traemos todos los div que tengan la clase click, en este caso todos los circulos
+const divs = document.querySelectorAll(".click");
+
+// Recorremos la lista divs para ver en cual fue donde dimos click
+divs.forEach(div => {
+  // Agregamos el add listener para ver cuál es el id del circulo donde dimos click
+  div.addEventListener("click", function () {
+    // Obtenemos el id
+    id = this.id;
+    // Traemos todos los items de los divs con la clase click
+    const elem = document.querySelectorAll('.elem');
+    // Por cada uno de los items reseteamos el color
+    elem.forEach(item => {
+      item.style.color = '';
+      item.style.backgroundColor = '';
+    });
+    // Traemos el circulo con el id que deseamos que es donde dimos click
+    const circle = document.getElementById(id);
+    // Traemos todos los div con la clase item del circulo de la línea anterior
+    const items = circle.querySelectorAll('.elem');
+
+    // Por cada uno de los items de la lista anterior les cambiamos color
+    items.forEach(item => {
+      item.style.color = 'red';
+      item.style.backgroundColor = 'yellow';
+    });
+  })
+})
+
+
+function revolverArr(array) {
+  let revuelto = array.slice();
+
+  for (let i = 0; i < revuelto.length; i++) {
+    const j = Math.floor(Math.random() * (1000)) % revuelto.length;
+    [revuelto[i], revuelto[j]] = [revuelto[j], revuelto[i]];
   }
-
-  return shuffledArray;
+  return revuelto;
 }
 
 function distributeItemsInCircle(items, id) {
@@ -26,68 +60,54 @@ function distributeItemsInCircle(items, id) {
   const radius = circle.offsetWidth / 2;
   const centerX = radius;
   const centerY = radius;
-  const angleStep = (2 * Math.PI) / items.length;
+  const sectorC = (2 * Math.PI) / items.length;
 
-  items = shuffleArray(items);
+  items = revolverArr(items);
 
   items.forEach((item, index) => {
-    const angle = index * angleStep;
+    const angle = index * sectorC;
     const x = centerX + radius * Math.cos(angle) - 15;
     const y = centerY + radius * Math.sin(angle) - 15;
 
-    const element = document.createElement('div');
-    element.classList.add('item');
-    element.textContent = item;
-    element.style.left = `${x}px`;
-    element.style.top = `${y}px`;
+    const num = document.createElement('div');
+    num.classList.add('elem');
+    num.textContent = item;
+    num.style.left = `${x}px`;
+    num.style.top = `${y}px`;
 
-    circle.appendChild(element);
+    circle.appendChild(num);
   });
 }
 
-// Nos basamos para hacer nuestra función
-// function rotateCircle(circleId, rotationSpeed) {
-//   const circle = document.getElementById(circleId);
-//   let rotationAngle = 0;
-//
-//   setInterval(() => {
-//     rotationAngle += rotationSpeed; // Incrementa el ángulo de rotación
-//     circle.style.transform = `rotate(${rotationAngle}deg)`; // Aplica la rotación
-//   }, 100); // Actualiza cada 100ms
-// }
 
-function rotateCircleButton(id, dir) {
-  // id_aux = shalala();
+function rotateCircleButton(dir) {
   const circle = document.getElementById(id);
-  console.log(id);
   switch (id) {
     case "cir1":
-      rotationAngle1 += (20 * dir);
+      rotationAngle1 += (360 / 6 * dir);
       circle.style.transform = `rotate(${rotationAngle1}deg)`;
       break;
 
     case "cir2":
-      rotationAngle2 += (20 * dir);
+      rotationAngle2 += (360 / 8 * dir);
       circle.style.transform = `rotate(${rotationAngle2}deg)`;
       break;
 
     case "cir3":
-      rotationAngle3 += (20 * dir);
+      rotationAngle3 += (360 / 10 * dir);
       circle.style.transform = `rotate(${rotationAngle3}deg)`;
       break;
 
     case "cir4":
-      rotationAngle4 += (20 * dir);
+      rotationAngle4 += (360 / 12 * dir);
       circle.style.transform = `rotate(${rotationAngle4}deg)`;
       break;
     case "cir5":
-      rotationAngle5 += (20 * dir);
+      rotationAngle5 += (360 / 14 * dir);
       circle.style.transform = `rotate(${rotationAngle5}deg)`;
       break;
   }
 }
-
-// rotateCircle("cir1", 3)
 
 distributeItemsInCircle(c1, "cir1")
 distributeItemsInCircle(c2, "cir2")
